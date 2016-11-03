858368bcfcd03451c8911a04b883c344fbcbdd01
Divjot Mehton <divjot65@gmail.com>
2016-11-03 19:57:41 +0000

    finsihed pokeapp

--- poke-js/controller.js
+++ poke-js/controller.js
@@ -12,7 +12,22 @@ pokeApp.controller('SearchController', ['$scope', 'pokeService', function($scope
         
         pokeService.get({name: $scope.pokeInput}, function(pokeObject){
             $scope.pokeName = pokeObject.name;
+            $scope.pokeHeight = pokeObject.height;
+            $scope.pokeWeight = pokeObject.weight;
+            $scope.pokeBasexp = pokeObject.base_experience;
+            
+            $scope.pokeSpeed = pokeObject.stats[0].base_stat;
+            $scope.pokeSecialDefense = pokeObject.stats[1].base_stat;
+            $scope.pokeSpecialAttack = pokeObject.stats[2].base_stat;
+            $scope.pokeDefense = pokeObject.stats[3].base_stat;
+            $scope.pokeAttack = pokeObject.stats[4].base_stat;
+            $scope.pokeHP = pokeObject.stats[5].base_stat;
+            
             $scope.pokeImg = pokeObject.sprites.front_default;
+            $scope.pokeImgbk = pokeObject.sprites.back_default;
+            $scope.pokeImg2 = pokeObject.sprites.front_shiny;
+            $scope.pokeImgbk2 = pokeObject.sprites.back_shiny;
+            
         });
     };
     
